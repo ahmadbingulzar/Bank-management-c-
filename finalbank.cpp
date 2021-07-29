ea772d089c27b232e3d2b45eb13f2c82b22bc3ed
ali <ahmadbingulzar@gmail.com>
2021-07-29 15:37:21 +0500

    changes

--- finalbank.cpp
+++ finalbank.cpp
@@ -7,7 +7,42 @@ remove account ,view total accounts list of users */
 #include"validations.h"
 #include<string>
 #include<regex>
+#include<algorithm>
 using namespace std;
+int menu();
+
+int account_number_check(int account_number_generated)
+{
+       string middle_name,last_name,city,country,month, first_name, date, year ,mobile,cnic_old;
+    int account_number;
+    fstream bank_data;
+    bank_data.open("bankdata.txt",ios::in);
+    while(!bank_data.eof()){ 
+ bank_data>>account_number>>cnic_old>>first_name>>middle_name>>last_name>>month>>date>>year>>city>>mobile>>country;
+     if(account_number==account_number_generated)
+    {
+        return true;
+    }
+    else false;
+       
+}
+}
+
+int cnic_test(string cnic)
+{
+    string middle_name,last_name,city,country,month, first_name, date, year ,mobile,cnic_old;
+    int account_number;
+    fstream bank_data;
+    bank_data.open("bankdata.txt",ios::in);
+    while(!bank_data.eof()){ 
+ bank_data>>account_number>>cnic_old>>first_name>>middle_name>>last_name>>month>>date>>year>>city>>mobile>>country;
+     if(cnic==cnic_old)
+    {
+        return true;
+    }
+    else false;
+}
+}
 //this funciton is used to view information of all the accounts
 void view_information()
 {
@@ -269,42 +304,55 @@ int update_data()
     remove("bankdata.txt");
     rename("temp.txt","bankdata.txt");
 }
-
 //this function is used to create new account
-char  create_new_account()
+void  create_new_account()
 {
     string middle_name,last_name,city,country,month, first_name, date, year ,cnic,mobile,cnic_old;
     int account_number;
     string months_array[12]={"jan","feb","march","april","may","june","july","aug","sep","oct","nov","dec"};
-    string date_array[31]={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"};
-    char choice;
+    string choice;
     string collect;
-   
     fstream bank_data;
     bank_data.open("bankdata.txt",ios::in|ios::app|ios::out);
+      srand(time(0));
+    account_number=rand()%10000;
+    while(account_number_check(account_number)==true){ 
     srand(time(0));
-    account_number=rand()%1000;
-    cout<<"This is your account number"<<account_number<<endl;
-        cout<<"Enter your cnic :";
-        cin>>cnic;
-   /* if(cnic==cnic_old)
-    {
-        cout<<"cnic already taken "<<endl;
-        return 0;
-    }*/
-    if(!isNumber(cnic))
-    { 
-        cout<<"Enter right choice"<<endl;
-        return 0;
-    }   
-    cout<<"Enter birth month"<<endl
+    account_number=rand()%10000;
+    account_number_check(account_number);
+    }
+    cout<<"Press m for main menu :";
+    cout<<"Enter your cnic without dashes and spaces :";
+    cin.ignore(256,'\n');
+    while(getline(cin,cnic)){ 
+       if(cnic=="m")
+        menu();
+    if(isNumber(cnic)==false)
+         cout<<"Enter right choice or Press m for main menu"<<endl;
+    else if(cnic_test(cnic)==true)
+        cout<<"cnic already taken enter again or Press m for main menu "<<endl;
+    else
+        break; 
+}
+ cout<<"Enter Date of birth year or Press m for main menu :";
+    while(getline(cin,year)){
+        if(year=="m")
+            menu();
+   else if(is_year(year))
+        break;
+    else
+        cout<<"Enter correct year again or Press m for the main menu :";
+   }
+    cout<<"Enter Date of birth month or Press m for main menu"<<endl
     <<"select month"<<endl
-    <<"jan\nfeb\nmar\napril\nmay\njun\njuly\naugust\nsep\noct\nnov\ndec"<<endl
+    <<"jan\nfeb\nmar\napril\nmay\njune\njuly\naugust\nsep\noct\nnov\ndec"<<endl
     <<"Enter your choice :";
     bool flag=false;
     int i;
     while(flag==false){ 
-        cin>>month;
+        getline(cin,month);
+        if(month=="m")
+            menu();
     for (i=0;i<12;i++)
     if(months_array[i]==month)
     {
@@ -312,96 +360,119 @@ char  create_new_account()
     }
     if(flag==false)
    {  
-    cout<<"Enter right choice"<<endl;
+    cout<<"Enter right choice again or Press m for the main menu"<<endl;
    }
    }
-    cout<<"Enter birth date"<<endl;
+    cout<<"Enter Date of birth or Press m for main menu"<<endl;
     bool check=false;
     while(check==false){
-    cin>>date;
-    for(i=0;i<31;i++)
-    if(date_array[i]==date)
-    {
+    getline(cin,date);
+    if(date=="m")
+        menu();
+        int num=stoi(date);
+    for(i=1;i<=31;i++)
+    if(i==num)
+     
+        if(date_check(year,month,date)==false){
+            check=false;
+        }
+    
+        else
       check=true;
-    }
     if(check==false){  
-    cout<<"Enter right choice"<<endl;
+    cout<<"Enter right choice again or Press m for the main menu"<<endl;
     }
     }
-    cout<<"Enter birth year :";
-    while(cin>>year){
-    if(is_year(year))
-        break;
-    else
-        cout<<"Enter correct year :";
-   }
-    cout<<"Enter your first name :";
-    cin.clear();
-    cin.ignore();
-    while(cin>>first_name){ 
+    cout<<"Enter your first name without spaces (10 character allowed only) or Press m for main menu:";
+    while(getline(cin,first_name)){ 
+        if(first_name=="m")
+            menu();
         if(is_alpha(first_name))
         break;
         else
-        cout<<"enter again :";
+        cout<<"Enter right choice again or press m for the main menu :";
     }
-    cout<<"Do you have middle name [y/n] :";
-    cin>>choice;
-    if(choice=='y'){ 
-    cin.ignore(256, '\n');
-    cout<<"Enter your middle name :";
-    while(cin>>middle_name){
+    cout<<"Do you have middle name [y/n] Press m for main menu :";
+    while(getline(cin,choice)){ 
+    if(choice=="m")
+        menu();
+    if(choice=="y"){ 
+    cout<<"Enter your middle name  without spaces (10 character allowed only) or Press m for main menu:";    
+    while(getline(cin,middle_name)){
+        if(middle_name=="m")
+            menu();
         if(is_alpha(middle_name))
             break;
     else
-        cout<<"Enter again :";
+        cout<<"Enter right choice again or Press m for the main menu:";
     }
-    }
-    else
-    cin.ignore(256, '\n');
-    cout<<"Enter your last name :";
-    while(cin>>last_name){
+    cout<<"Enter your Last name without spaces (10 character allowed only) or Press m for main menu:";
+    while(getline(cin,last_name)){
+        if(last_name=="m")
+            menu();
     if(is_alpha(last_name))
         break;
     else
-        cout<<"Enter again :";
+        cout<<"Enter right choice again [y/n] or Press m for the main menu :";
     }
-    cin.ignore(256, '\n');
-    cout<<"Enter your city name :";
-    while(cin>>city){
-    if(is_alpha(city))
+    break;
+}
+    else if(choice=="n"){ 
+    cout<<"Enter your Last name without spaces (10 character allowed only) or Press m for main menu :";
+    while(getline(cin,last_name)){
+    if(last_name=="m")
+        menu();
+    if(is_alpha(last_name))
         break;
     else
-        cout<<"Enter again :";
+        cout<<"Enter right choice again or Press m for the main menu:";
     }
-    cin.ignore(256, '\n');
-    cout<<"Enter your mobile number :";
-    while(cin>>mobile){
+    break;
+}
+    else
+    cout<<"Enter right choice again or Press m for the main menu:";
+}
+    cout<<"Enter your City name without spaces (20 character allowed only)or Press m for main menu :";
+    while(getline(cin,city)){
+        if(city=="m")
+            menu();
+    if(is_city(city))
+        break;
+    else
+        cout<<"Enter right choice again or Press m for the main menu:";
+    }
+    cout<<"Enter your mobile number or Press m for main menu :";
+    while(getline(cin,mobile)){
+        if(mobile=="m")
+            menu();
     if(is_phone(mobile))
         break;
     else
-        cout<<"Enter again :";
+        cout<<"Enter right choice again or Press m for the main menu :";
     }
-    cin.ignore(256, '\n');
-    cout<<"Enter country name :";
-    while(cin>>country){
+    
+  /* cout<<"Enter country name or Press m for main menu :";
+    while(getline(cin,country)){
+        if(country=="m")
+            menu();
     if(is_alpha(country))
         break;
     else  
-        cout<<"Enter again :";
-    }
-    if(choice=='y')
+        cout<<"Enter again without spaces or Press m for the main menu :";
+    }*/
+    if(choice=="y")
     {
-    bank_data<<account_number<<"\t"<<cnic<<"\t"<<first_name<<"\t"<<middle_name<<"\t"<<last_name<<"\t"<<month<<"\t"<<date<<"\t"<<year<<"\t"<<city<<"\t"<<mobile<<"\t"<<country<<endl;
+    bank_data<<account_number<<"\t"<<cnic<<"\t"<<first_name<<"\t"<<middle_name<<"\t"<<last_name<<"\t"<<month<<"\t"<<date<<"\t"<<year<<"\t"<<city<<"\t"<<mobile<<"\t"<<"Pakistan"<<endl;
+    cout<<"This is your account number :"<<account_number<<endl;
     cout<<"Account created sucessfully"<<endl;
     }
     else
     {
-        bank_data<<account_number<<"\t"<<cnic<<"\t"<<first_name<<"\t"<<"NULL"<<"\t"<<last_name<<"\t"<<month<<"\t"<<date<<"\t"<<year<<"\t"<<city<<"\t"<<mobile<<"\t"<<country<<endl;
+    bank_data<<account_number<<"\t"<<cnic<<"\t"<<first_name<<"\t"<<"NULL"<<"\t"<<last_name<<"\t"<<month<<"\t"<<date<<"\t"<<year<<"\t"<<city<<"\t"<<mobile<<"\t"<<"Pakistan"<<endl;
+    cout<<"This is your account number :"<<account_number<<endl;
     cout<<"Account created sucessfully"<<endl;
-    }
-   
+    }  
 }
-
 /*this is the menu function it displays the menu for the bank management system to user to select
 any option*/
 int menu()
@@ -410,60 +481,60 @@ int menu()
     string choice;
     int flag=0;
     while( true){ 
-    cout<<"CUSTOMER ACCOUNT BANKING MANAGEMENT SYSTEM "<<endl
-    <<"||||||| WELCOME TO THE MAIN MENU|||||||"<<endl
-    <<"1:Create new account"<<endl
-    <<"2:view information of all the accounts"<<endl
-    <<"3:Update Address and Phone number"<<endl
-    <<"4:View detail of a particular customer"<<endl
-    <<"5:Delete Account"<<endl
-    <<"6:Exit"<<endl
-    <<endl
-    <<endl
-    <<"Enter your choice: "<<endl;
-    cin>>choice;
-    if(choice=="1")
-    {
-        validity_count=0;
-        create_new_account();
+        cout<<"CUSTOMER ACCOUNT BANKING MANAGEMENT SYSTEM "<<endl
+        <<"||||||| WELCOME TO THE MAIN MENU|||||||"<<endl
+        <<"1:Create new account"<<endl
+        <<"2:view information of all the accounts"<<endl
+        <<"3:Update Address and Phone number"<<endl
+        <<"4:View detail of a particular customer"<<endl
+        <<"5:Delete Account"<<endl
+        <<"6:Exit"<<endl
+        <<endl
+        <<endl
+        <<"Enter your choice: "<<endl;
+        cin>>choice;
+        if(choice=="1")
+        {
+            validity_count=0;
+            create_new_account();
+        }
+        else if(choice=="2")
+        {
+            validity_count=0;
+            view_information();
+        }
+        else if(choice=="3")
+        {
+            validity_count=0;
+            update_data();
+        }
+        else if(choice=="4")
+        {
+            validity_count=0;
+            view_customer_detail();
+        }
+        else if(choice=="5")
+        {
+            validity_count=0;
+            remove_account();
+        }
+        else if(choice=="6")
+        {
+            exit();
+            return 0;
+        }
+        else if(choice.length()!=1)
+        { 
+            validity_count++;
+            cout<<"wrong choice"<<endl;
+        }
+        if(validity_count==3)
+        { 
+            cout<<"Get out "<<endl;
+            break;
+        //return 0;
+        }
     }
-    else if(choice=="2")
-    {
-        validity_count=0;
-        view_information();
-    }
-    else if(choice=="3")
-    {
-         validity_count=0;
-        update_data();
-    }
-    else if(choice=="4")
-    {
-         validity_count=0;
-        view_customer_detail();
-    }
-    else if(choice=="5")
-    {
-        validity_count=0;
-        remove_account();
-    }
-    else if(choice=="6")
-    {
-        exit();
-        return 0;
-    }
-    else if(choice.length()!=1)
-    { 
-        validity_count++;
-        cout<<"wrong choice"<<endl;
-    }
-    if(validity_count==3)
-    { 
-        cout<<"Get out "<<endl;
-        break;
-     //return 0;
-    }
-}
 }
 //this is the main function of the program 
 int main()
--- validations.h
+++ validations.h
@@ -2,8 +2,9 @@
 #include<algorithm>
 #include<regex>
 #include<ctime>
-
 using namespace std;
+
+bool leapYear(string year);
 bool isNumber(string cnic)
 {
 	if(cnic.length()==13){ 
@@ -17,33 +18,124 @@ bool isNumber(string cnic)
 }
 bool is_year(string year)
 {
+	  for (int i = 0; i < year.length(); i++)
+        if (isdigit(year[i]) == false)
+        return false;
 	int num=stoi(year);
 	 time_t t = time(NULL);
  tm* tPtr = localtime(&t);
 
-if(num>=1000&&num<=(tPtr->tm_year)+1900)
+if(num>=1905 && num<(tPtr->tm_year)+1900 && (tPtr->tm_year)+1900-num>=18)
 	return true;
 	else
 	return false;
 }
-bool is_alpha(string first_name)
+bool is_alpha(string name)
 {
-if(regex_match(first_name,regex("^[A-Za-z]+$")))
+if(regex_match(name,regex("^[A-Za-z]+$")) && name.length()<=10)
 return true;
 return false;
-	
+}
+
+
+bool is_city(string name)
+{
+if(regex_match(name,regex("^[A-Za-z]+$")) && name.length()<=20)
+return true;
+return false;
 }
 bool is_phone(string phone)
 {
 	if(regex_match(phone,regex("(0|91)?[3][0-9]{9}")))
 	return true;
 	return false;
-
 }
 bool is_valid_entry(string account_number)
 {
-	if(regex_match(account_number,regex("[1-1000]{3,5}")))
+	if(regex_match(account_number,regex("[1-100000000]{4}")))
 	return true;
 	return false;
 }
+int date_check(string year,string month,string date)
+{
+    int num=stoi(date);
+	if(month=="april"&& num==31)
+	{
+		return false;
+
+	}
+	if(month=="june"&& num==31)
+	{
+		return false;
+	}
+	if(month=="sep"&& num==31)
+	{
+		return false;
+	}
+	if(month=="nov"&& num==31)
+	{
+		return false;
+	}
+		if(month=="jan"&& num==31)      // January, March, May, July, August, October, and December.
+	{
+		return true;
+	}
+	if(month=="mar"&&num==31)
+	{
+		return true;
+	}
+	if(month=="may"&& num==31)
+	{
+		return true;
+	}
+	if(month=="july"&& num==31)
+	{
+		return true;
+	}
+		if(month=="aug"&& num==31)
+	{
+		return true;
+	}	
+	if(month=="oct"&& num==31)
+	{
+		return true;
+	}	
+	if(month=="dec"&& num==31)
+	{
+		return true;
+	}
+	if(leapYear(year)==true&&month=="feb"&&num==29)
+	{
+		return true;
+	}
+	if(month=="feb"&& num==29||num==30||num==31)
+	{
+		return false;
+	}
+	else
+	return true;
+	
+}
+bool leapYear(string year){ 
+	   //leap year condition
+	   int num=stoi(year);
+   if (num % 4 == 0)
+   {
+      if (num % 100 == 0)
+      {
+         if(num % 400 == 0)
+            return true;
+         else
+            return false;
+      }
+      else
+         return true;
+   }
+   else
+      return false;
+
+   
+}
+   
+   
 
