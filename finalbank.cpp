0a81bdcfe35f0f33c2523e9afc7dcc1f4b68a83c
ali <ahmadbingulzar@gmail.com>
2021-07-30 00:38:46 +0500

    view one customer detail function is added to a header file

--- finalbank.cpp
+++ finalbank.cpp
@@ -9,44 +9,6 @@ remove account ,view total accounts list of users */
 #include<regex>
 #include<algorithm>
 using namespace std;
-//this funciton is used to view information of all the accounts
-void view_information()
-{
-    string file_read;
-    ifstream read;
-    read.open("bankdata.txt",ios::in);
-    while(!read.eof())
-    {
-        getline(read,file_read);
-        cout<<file_read<<endl;
-    }  
-}
-
-//this function is used to view the information of a particular account
-int view_customer_detail()
-{
-    string middle_name,last_name,city,country,month, first_name, date, year ,cnic,mobile, account_number, account_number_check;
-    bool flag=false;
-    ifstream check;
-    check.open("bankdata.txt",ios::in);
-    cout<<"enter account number :";
-    cin>>account_number_check;
-    while(!check.eof())
-    {
-        check>>account_number>>cnic>>first_name>>middle_name>>last_name>>month>>date>>year>>city>>mobile>>country;    
-        if(account_number_check==account_number)
-        {
-            flag=true;
-            cout<<account_number<<"\t"<<cnic<<"\t"<<first_name<<"\t"<<middle_name<<"\t"<<last_name<<"\t"<<month<<"\t"<<date<<"\t"<<year<<"\t"<<city<<"\t"<<mobile<<"\t"<<country;
-            break;
-        }
-    }
-    if(flag==false)
-        cout<<"No Record Available :"<<endl;    
-        return 0;
-}
-
-
 //this is the main function of the program 
 int main()
 {
---
+++ view_information.h
@@ -0,0 +1,12 @@
+//this funciton is used to view information of all the accounts
+void view_information()
+{
+    string file_read;
+    ifstream read;
+    read.open("bankdata.txt",ios::in);
+    while(!read.eof())
+    {
+        getline(read,file_read);
+        cout<<file_read<<endl;
+    }  
+}
