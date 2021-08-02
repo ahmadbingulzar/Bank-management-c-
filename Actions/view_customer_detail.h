f34e8448c86ba653368149786d0f1508f041d75d
ali <ahmadbingulzar@gmail.com>
2021-08-02 12:41:52 +0500

    view particular record header has been done with tha validations and loop input

--- Actions/view_customer_detail.h
+++ Actions/view_customer_detail.h
@@ -1,23 +1,36 @@
-//this function is used to view the information of a particular account
-int view_customer_detail()
+bool entry_check(string account_number_check)
 {
-    string middle_name, last_name, city, country, month, first_name, date, year, cnic, mobile, account_number, account_number_check;
-    bool flag = false;
     ifstream check;
     check.open("bankdata.txt", ios::in);
-    cout << "enter account number :";
-    cin >> account_number_check;
+
+    string middle_name, last_name, city, country, month, first_name, date, year, cnic, mobile, account_number;
+
     while (!check.eof())
     {
         check >> account_number >> cnic >> first_name >> middle_name >> last_name >> month >> date >> year >> city >> mobile >> country;
         if (account_number_check == account_number)
         {
-            flag = true;
             cout << account_number << "\t" << cnic << "\t" << first_name << "\t" << middle_name << "\t" << last_name << "\t" << month << "\t" << date << "\t" << year << "\t" << city << "\t" << mobile << "\t" << country;
-            break;
+            return true;
         }
     }
-    if (flag == false)
-        cout << "No Record Available :" << endl;
-    return 0;
+}
+//this function is used to view the information of a particular account
+int view_customer_detail()
+{
+    string account_number_check;
+    cin.ignore(256, '\n');
+    cout << "Enter account number or Press m for main menu without spaces :";
+    while (true)
+    {
+        
+        getline(cin,account_number_check);
+        if (account_number_check == "m")
+            menu();
+        if (entry_check(account_number_check) == true)
+            cout << "search again or Press m for main menu without spaces :";
+
+        else
+            cout << "No Record Available enter again or Press m for main menu without spaces :";
+    }
 }
